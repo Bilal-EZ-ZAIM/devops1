9f4bb98036abd221bdf9ab01720873f64d36facb
Bilal-EZ-ZAIM <bilalzail499@gmail.com>
2024-11-23 10:31:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bonjour, le serveur fonctionne !');
+});
+
+app.listen(5000, () => {
+  console.log('Le serveur écoute sur le port 5000');
+});
