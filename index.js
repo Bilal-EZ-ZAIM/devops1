900b06fe7688653e4c00e30cfd1c2c75b485cf70
Bilal-EZ-ZAIM <bilalzail499@gmail.com>
2024-11-23 12:31:01 +0100

    ajouter

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.send("Bonjour, le serveur fonctionne !");
+  const users = [
+    { id: 1, name: "Alice", email: "alice@example.com" },
+    { id: 2, name: "Bob", email: "bob@example.com" },
+    { id: 3, name: "Charlie", email: "charlie@example.com" },
+    { id: 4, name: "David", email: "david@example.com" },
+    { id: 5, name: "Eve", email: "eve@example.com" },
+  ];
+
+  // Renvoyer la liste des utilisateurs en réponse
+  res.status(200).json(users);
 });
 
 app.get("/api/users", (req, res) => {
